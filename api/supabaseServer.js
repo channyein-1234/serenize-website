6488404f4064aff951d13e3be1adb43a1adc37e4
channyein-1234 <channyeinsumon@gmail.com>
2025-06-13 02:58:25 +0800

    added dotenv

--- api/supabaseServer.js
+++
@@ -1,9 +0,0 @@
-// supabaseAdmin.js (or supabaseServer.js)
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://jvfpbjoxilxcxezyxcnk.supabase.co';
-const supabaseServiceRoleKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2ZnBiam94aWx4Y3hlenl4Y25rIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NTUwNzY2MSwiZXhwIjoyMDYxMDgzNjYxfQ.P4jLG98y3wg5yUMUf-mv2yUMnWxWGQst54S7HDnAjZU';
-
-const supabaseAdmin = createClient(supabaseUrl, supabaseServiceRoleKey);
-
-export default supabaseAdmin;
